c9526594e9ac0b3ca503d3be3c3d3401b91cc5ce
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-07-28 16:49:34 +0000

    Piece of the fix for internships database; data mapping was off

--- section-careerservices.php
+++ section-careerservices.php
@@ -11,10 +11,10 @@ Template Name: Section - Career Services
   <div id="sidebar">
 <ul id="sidebar-nav">
   <li><h4><a href="<?php bloginfo('url'); ?>/career-services/">Career Services</a></h4></li>
-  <li><a href="<?php bloginfo('url'); ?>/career-services/news-events/">News & Events</a></li>
+  <li><a href="<?php bloginfo('url'); ?>/career-services/news-events/">News &amp; Events</a></li>
   <li><a href="<?php bloginfo('url'); ?>/category/career-services/internships/">Internship Listings</a></li>
   <li><a href="<?php bloginfo('url'); ?>/career-services/freelance-community-publications/">Freelance: Community Publications</a></li>
-  <li><a href="<?php bloginfo('url'); ?>/career-services/job-hunting-career-info-links/">Job-Hunting & Career-Info Links</a></li>
+  <li><a href="<?php bloginfo('url'); ?>/career-services/job-hunting-career-info-links/">Job-Hunting &amp; Career-Info Links</a></li>
   <li><a href="<?php bloginfo('url'); ?>/career-services/professional-organizations/">Professional Organizations</a></li>
   <li><a href="<?php bloginfo('url'); ?>/career-services/for-employers/">For Employers</a></li>
   <li><a href="<?php bloginfo('url'); ?>/career-services/where-our-alumni-are-working/">Where Our Alumni Are Working</a></li>
@@ -34,13 +34,13 @@ Template Name: Section - Career Services
 
 <p><strong>Deborah Stead</strong><br />
 Director of Career Services<br />
-646-758-7732<br />
-<a href="mailto:deborah.stead@journalism.cuny.edu">deborah.stead@journalism.cuny.edu</a></p>
+<a href="mailto:deborah.stead@journalism.cuny.edu">deborah.stead@journalism.cuny.edu</a><br />
+646-758-7732<br /></p>
 
 <p><strong>Lili Grossman</strong><br />
 Career Services Coordinator<br />
-646-758-7727<br />
-<a href="mailto:lili.grossman@journalism.cuny.edu">lili.grossman@journalism.cuny.edu</a></p>
+<a href="mailto:lili.grossman@journalism.cuny.edu">lili.grossman@journalism.cuny.edu</a><br />
+646-758-7727<br /></p>
 
 <p><strong>Office Hours</strong><br />
 9 a.m.-5 p.m., Monday-Friday</p>
