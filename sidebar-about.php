892a3790ce78d2a428235553d061d54563ae5458
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-07-29 21:48:14 +0000

    New sidebar

---
+++ sidebar-about.php
@@ -0,0 +1,24 @@
+<div id="sidebar">
+<ul id="sidebar-nav">
+<li><h4><a href="<?php bloginfo('url'); ?>/about/">About</a></h4></li>
+<li><a href="<?php bloginfo('url'); ?>/about/deans-corner/">Dean's Corner</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/board-of-advisers/">Board of Advisers</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/code-of-ethics/">Code of Ethics</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/campus-facilities/">Campus and Facilities</a>
+  <ul>
+    <li><a href="<?php bloginfo('url'); ?>/about/campus-facilities/campus-safety/">Campus Safety</a></li>
+    <li><a href="<?php bloginfo('url'); ?>/about/campus-facilities/hours-of-operation/">Hours of Operation</a></li>
+  </ul>
+</li>
+<li><a href="<?php bloginfo('url'); ?>/about/cuny-tv/">CUNY TV</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/guest-information/">Guest Information</a>
+  <ul>
+    <li><a href="<?php bloginfo('url'); ?>/about/guest-information/map-and-directions/">Map and Directions</a></li>    
+  </ul>
+</li>
+<li><a href="<?php bloginfo('url'); ?>/about/logo-and-graphics-standards/">Logos and Graphics Standards</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/undergraduate-programs-events/">Undergraduate Programs &amp; Events</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/room-request/">Room Request</a></li>
+<li><a href="<?php bloginfo('url'); ?>/about/freedom-of-information-law-notice/">Freedom of Information Law Notice</a></li>
+</ul>
+</div>
