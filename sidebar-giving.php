f21445d8ec29da2102565cbb7167a7f386c5d354
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-07-30 16:12:36 +0000

    Even more page consolidation

---
+++ sidebar-giving.php
@@ -0,0 +1,25 @@
+<div id="sidebar">
+
+<ul id="give-now">
+<li><h4><a href="https://cunyjschool.wufoo.com/forms/q7p7r3/">DONATE NOW!</a></li>
+</ul>
+
+<ul id="sidebar-nav">
+<li><h4><a href="<?php bloginfo('url'); ?>/donate/">Donate</a></h4></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/ways-to-give/">Ways to Give</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/planned-giving/">Planned Giving</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/matching-gifts/">Matching Gifts</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/program-enhancements-and-projects/">Program Enhancements and Projects</a></li>
+<li><h3 style="margin-top: 10px;">Established Funds</h3></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/alumni-fund/">Alumni Fund</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/future-journalists-fund/">Future Journalists Fund</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/annual-fund/">Annual Fund</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/named-funds-special-programs-prizes-and-awards/">Named Funds, Special Programs, Prizes, and Awards</a></li>
+<li><h3 style="margin-top: 10px;">Stay Connected</h3></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/special-events/">Special Events</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/sign-up-to-receive-our-newsletter/">Sign Up for our Newsletter</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/update-your-contact-information/">Update Your Contact Information</a></li>
+<li><a href="<?php bloginfo('url'); ?>/donate/institutional-advancement-staff/">Institutional Advancement Staff</a></li>
+</ul>
+
+</div>
