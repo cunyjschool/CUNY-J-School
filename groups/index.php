6ce088247f2d273ff33f213fc63b82d7f925ec4d
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-19 19:55:23 +0000

    Removed Groups header and create button on directory

--- groups/index.php
+++ groups/index.php
@@ -9,7 +9,6 @@
 	<div id="groups_directory" class="content directory">
 
 		<form action="" method="post" id="groups-directory-form" class="dir-form">
-			<h2><?php if ( is_user_logged_in() ) : ?><a class="create button" href="<?php echo bp_get_root_domain() . '/' . BP_GROUPS_SLUG . '/create/' ?>"><?php _e( 'Create a Group', 'buddypress' ) ?></a><?php endif; ?>Groups</h2>
 				
 			<div id="primary-search">
 				<?php include (TEMPLATEPATH . '/groups/searchform.php'); ?>
