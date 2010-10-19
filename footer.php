b504de7475ae11a2e6d1714ba26d1a3c8b93a138
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-19 21:26:12 +0000

    Fix Clicky analytics check

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@ http://assistive.usablenet.com/tt/<?php bloginfo('url'); ?>">Text-Only</a>
   </div>
 
 <!-- Clicky analytics tag -->
-<?php if ( get_bloginfo('url') == 'www.journalism.cuny.edu' ) : ?>
+<?php if ( get_bloginfo('url') == 'http://www.journalism.cuny.edu' ) : ?>
 	<script src="http://static.getclicky.com/js" type="text/javascript"></script>
 	<script type="text/javascript">clicky.init(249402);</script>
 	<noscript><p style="display:none;"><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/249402ns.gif" /></p></noscript>
