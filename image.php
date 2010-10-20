902f2d85b8a040a7e8bdb47728ec3a806973bca3
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-20 19:46:13 +0000

    Finished styling single landing page for media assets

--- image.php
+++ image.php
@@ -2,33 +2,52 @@
 
 	<div class="wrap">
 		
-	<div class="main">
+	<div class="main" id="image-single">
 
-	<div id="content" class="widecolumn">
+	<div class="content">
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div class="image" id="image-<?php the_ID(); ?>">
-			
+		
+			<div class="parent-link"><a href="<?php echo get_permalink($post->post_parent); ?>">&larr; <?php echo get_the_title($post->post_parent); ?></a></div>
 			<h2><?php the_title(); ?></h2>
-			<div class=""><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment">Back to <?php echo get_the_title($post->post_parent); ?></a></div>
-			<div class="entry">
-				<a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, array( 845, 845 ) ); ?></a>
-                <div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?></div>
-
-				<?php the_content(); ?>
-
-				<div class="navigation">
-					<div class="alignleft"><?php previous_image_link() ?></div>
-					<div class="alignright"><?php next_image_link() ?></div>
+			
+			<div class="primary-image"><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, array( 850, 850 ) ); ?></a></div>
+			<?php if ( $img_meta = wp_get_attachment_metadata( $post->id ) ) {
+				echo '<div class="image-meta">';
+				$html = '';
+				if ( $img_meta['image_meta']['credit'] ) {
+					$html .= 'Photo by ' . $img_meta['image_meta']['credit'] . ' | ';
+				}
+				if ( $img_meta['image_meta']['aperture'] && $img_meta['image_meta']['shutter_speed'] ) {
+					$html .= $img_meta['image_meta']['shutter_speed'] . ' shutter at f/' . $img_meta['image_meta']['aperture'] . ' and ISO ' . $img_meta['image_meta']['iso'];
+				}
+				echo rtrim( $html, '| ' );
+				echo edit_post_link( 'Edit this image', ' | ', '' );
+				echo '</div>';
+			} ?>
+			
+			<?php if ( !empty( $post->post_excerpt ) ) : ?>
+			<div class="image-caption"><?php the_excerpt(); ?></div>
+			<?php endif; ?>
+			
+			<?php if ( !empty( $post->post_content ) ) : ?>			
+			<div class="image-description"<?php the_content(); ?></div>
+			<?php endif; ?>
+			
+			<div class="navigation">
+				<div class="left-navigation navigation-link">
+					<?php previous_image_link( array( 64, 64 ) ); ?><p><?php previous_image_link( false, '&larr; Previous' ); ?>
 				</div>
-				<br class="clear" />
-
-				<?php edit_post_link('Edit this entry.','',''); ?>
-
+				<div class="right-navigation navigation-link">
+					<?php next_image_link( array( 64, 64 ) ); ?><p><?php next_image_link( false, 'Next &rarr;' ); ?>
+				</div>
 			</div>
+			
+			<div style="clear:both;"></div>
 
-		</div>
+		</div><!-- END - .image -->
 
 	<?php // comments_template(); ?>
 
@@ -36,12 +55,12 @@
 
 		<div class="message info">Sorry, no attachments matched your criteria.</div>
 
-<?php endif; ?>
+	<?php endif; ?>
 
-	</div>
+	</div><!-- END - .content -->
 	
-	</div>
+	</div><!-- END - .main -->
 	
-	</div>
+</div><!-- END - .wrap -->
 
 <?php get_footer(); ?>
