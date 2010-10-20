027affa4d17c9e32579268a3e678d33a4ced9e99
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-20 19:27:44 +0000

    Resolve conflicting styles

--- home.php
+++ home.php
@@ -4,9 +4,9 @@
 	
 	<!-- Pingdom check -->
 	
-	<div class="main">
+	<div class="main" id="home">
 	
-	 <div id="content">
+	 <div class="content">
 		
 		<?php /* <div id="home-alert">
 			<strong>Today and tomorrow:</strong> SABEW's 2010 Fall Conference, happening at the J-School. <a href="http://cunyatsabew.com/">Follow the coverage</a> or <a href="http://www.journalism.cuny.edu/live/">watch the livestream</a>.
