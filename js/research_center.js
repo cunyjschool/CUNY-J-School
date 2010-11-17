46c22e4abcadcd13b3bab753c79c995e88938bd5
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-11-17 18:59:31 +0000

    Only load posts if div exists

--- js/research_center.js
+++ js/research_center.js
@@ -4,29 +4,34 @@
  */
 function cunyj_load_research_blog_posts( url, items, zone ) {
 	
-	url = url + 'api/get_recent_posts/?count=' + items;
+	// Only make the AJAX request if the zone exists
+	if ( jQuery('#'+zone).length > 0 ) {
 	
-	jQuery.ajax({
-		url: url,
-		dataType: 'jsonp',
-		success: function(data) {
-			var html = '';
-			// Only add data if the status has returned 'ok'
-			if ( data.status == 'ok' ) {
-				html += '<ul>';
-				jQuery.each(data.posts, function( index, item ) {
-					html += '<li><a href="' + item.url + '">' + item.title + '</a>';
-					//html += '<br />By ' + item.author.name;
-					html += '</li>';
-				})
-				html += '</ul>';			
-			} else {
-				html += '<ul><li>Error loading feed</li></ul>';
-			}
-			jQuery('#'+zone+' ul').remove();			
-			jQuery('#'+zone).append(html);
-		},
-	});
+		url = url + 'api/get_recent_posts/?count=' + items;
+	
+		jQuery.ajax({
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				var html = '';
+				// Only add data if the status has returned 'ok'
+				if ( data.status == 'ok' ) {
+					html += '<ul>';
+					jQuery.each(data.posts, function( index, item ) {
+						html += '<li><a href="' + item.url + '">' + item.title + '</a>';
+						//html += '<br />By ' + item.author.name;
+						html += '</li>';
+					})
+					html += '</ul>';			
+				} else {
+					html += '<ul><li>Error loading feed</li></ul>';
+				}
+				jQuery('#'+zone+' ul').remove();			
+				jQuery('#'+zone).append(html);
+			},
+		});
+		
+	}
 }
 
 jQuery(document).ready(function() {
