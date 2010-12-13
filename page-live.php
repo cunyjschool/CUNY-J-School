560fa300a2a437be86eb1490d9a58ef7002f870f
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-12-13 21:44:47 +0000

    Work on widening the live page

--- page-live.php
+++ page-live.php
@@ -4,7 +4,53 @@ Template Name: Page - Live
 */
 ?>
 
-<?php get_header(); ?>
+<?php 
+	$theme_details = get_theme_data(get_bloginfo('template_directory') . '/style.css');
+	$theme_version = $theme_details['Version'];
+	$cunyj = new cunyj();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+	
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="description" content="Streaming content from the CUNY Graduate School of Journalism" />
+
+	<meta name="copyright" content="Copyright <?php echo date('Y'); ?> City University of New York Graduate School of Journalism" />
+	<meta http-equiv="content-language" content="en" />
+
+	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+
+	<?php
+	/**
+	 * All stylesheets are enqueued in functions.php
+	 */
+	?>
+
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" type="image/x-icon" />
+
+	<?php wp_head(); ?>
+	
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/live.css" />
+  
+</head>
+
+<body id="live">
+
+<div id="tile-orange"></div>
+
+<div id="global">
+	<div class="wrap">
+ 		<div id="logo" class="minimal">
+	      <a title="<?php bloginfo('title'); ?>" href="<?php bloginfo('url'); ?>"><img alt="CUNY Graduate School of Journalism" src="<?php bloginfo('template_directory'); ?>/images/logos/cunyj-logo_h360.png" height="45px" /></a>
+	    </div>
+
+	</div><!-- /.wrap -->
+</div><!-- /#global -->
 
 <div class="wrap">
 	
