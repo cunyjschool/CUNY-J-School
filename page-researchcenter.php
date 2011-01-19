b7240bfdb5d7fe174756baf78f612d4907deee3b
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-01-19 22:21:30 +0000

    Move the RC tabber above the page navigation and add styles

--- page-researchcenter.php
+++ page-researchcenter.php
@@ -14,6 +14,74 @@ Template Name: Page - Research Center
 	
 		<img class="ribbon" src="<?php bloginfo('template_directory'); ?>/images/pages/researchcenter_h850.jpg" alt="Research Center entrance" height="100px" width="850px" />
 		
+		<?php if ( is_page( 'research-center' ) ) : ?>
+
+		<div id="research-center-blog" class="research-center-info-zone">
+			<h4 class="blog-header"><a href="http://researchcenter.journalism.cuny.edu/">Research Center Blog</a></h4>
+			<ul>
+				<li>Loading...</li>
+			</ul>
+		</div>
+
+		<div class="tabber" id="research-center-tabber">
+			<?php
+				$tabs_fields = array();
+				$tabs_fields['databases'] = get_post_meta( $post->ID, 'rc_databases_tab', true );
+				$tabs_fields['subject_guides'] = get_post_meta( $post->ID, 'rc_subject_guides_tab', true );
+				$tabs_fields['journals'] = get_post_meta( $post->ID, 'rc_journals_tab', true );
+				$tabs_fields['books'] = get_post_meta( $post->ID, 'rc_books_tab', true );
+				$tabs_fields['ebooks'] = get_post_meta( $post->ID, 'rc_ebooks_tab', true );
+
+				$tabs_navigation = '';
+				$tabs_content = '';
+				if ( count( $tabs_fields ) ) {
+					foreach( $tabs_fields as $key => $value ) {
+						if ( $value ) {
+							$tabs_navigation .= '<li>';
+							switch( $key ) {
+								case 'databases':
+									$tabs_navigation .= 'Databases';
+									$tabs_content .= '<div class="tabber-item" id="databases-tabber-item">' . $value . '</div>';							
+									break;
+								case 'subject_guides':
+									$tabs_navigation .= 'Subject Guides';
+									$tabs_content .= '<div class="tabber-item" id="subject-guides-tabber-item">' . $value . '</div>';
+									break;
+								case 'journals':
+									$tabs_navigation .= 'Journals/Periodicals';
+									$tabs_content .= '<div class="tabber-item" id="journals-tabber-item">' . $value . '</div>';							
+									break;
+								case 'books':
+									$tabs_navigation .= 'Books';
+									$tabs_content .= '<div class="tabber-item" id="books-tabber-item">' . $value . '</div>';							
+									break;	
+								case 'ebooks':
+									$tabs_navigation .= 'E-Books';
+									$tabs_content .= '<div class="tabber-item" id="ebooks-tabber-item">' . $value . '</div>';							
+									break;																							
+								default:
+									$tabs_navigation .= 'Unknown';
+									break;
+							}
+							$tabs_navigation .= '</li>';					
+						}
+					} // END - foreach( $tabs_content as $key => $value )
+				}
+
+				// Only print the tabs functionality if the buttons are set
+				if ( $tabs_navigation ) {
+					echo '<ul class="tabber-navigation">' . $tabs_navigation . '</ul>';
+					echo $tabs_content;
+				}
+
+			?>
+
+		</div>
+
+		<div style="clear:both;"></div>
+		
+		<?php endif; ?>
+		
 	<div class="sidebar left standard">
 
 		<?php
@@ -40,71 +108,6 @@ Template Name: Page - Research Center
 	<div class="page">
 		
 	<?php if ( is_page( 'research-center' ) ) : ?>
-	
-	<div id="research-center-blog" class="research-center-info-zone">
-		<h4 class="blog-header"><a href="http://researchcenter.journalism.cuny.edu/">Research Center Blog</a></h4>
-		<ul>
-			<li>Loading...</li>
-		</ul>
-	</div>
-	
-	<div class="tabber" id="research-center-tabber">
-		<?php
-			$tabs_fields = array();
-			$tabs_fields['databases'] = get_post_meta( $post->ID, 'rc_databases_tab', true );
-			$tabs_fields['subject_guides'] = get_post_meta( $post->ID, 'rc_subject_guides_tab', true );
-			$tabs_fields['journals'] = get_post_meta( $post->ID, 'rc_journals_tab', true );
-			$tabs_fields['books'] = get_post_meta( $post->ID, 'rc_books_tab', true );
-			$tabs_fields['ebooks'] = get_post_meta( $post->ID, 'rc_ebooks_tab', true );
-			
-			$tabs_navigation = '';
-			$tabs_content = '';
-			if ( count( $tabs_fields ) ) {
-				foreach( $tabs_fields as $key => $value ) {
-					if ( $value ) {
-						$tabs_navigation .= '<li>';
-						switch( $key ) {
-							case 'databases':
-								$tabs_navigation .= 'Databases';
-								$tabs_content .= '<div class="tabber-item" id="databases-tabber-item">' . $value . '</div>';							
-								break;
-							case 'subject_guides':
-								$tabs_navigation .= 'Subject Guides';
-								$tabs_content .= '<div class="tabber-item" id="subject-guides-tabber-item">' . $value . '</div>';
-								break;
-							case 'journals':
-								$tabs_navigation .= 'Journals/Periodicals';
-								$tabs_content .= '<div class="tabber-item" id="journals-tabber-item">' . $value . '</div>';							
-								break;
-							case 'books':
-								$tabs_navigation .= 'Books';
-								$tabs_content .= '<div class="tabber-item" id="books-tabber-item">' . $value . '</div>';							
-								break;	
-							case 'ebooks':
-								$tabs_navigation .= 'E-Books';
-								$tabs_content .= '<div class="tabber-item" id="ebooks-tabber-item">' . $value . '</div>';							
-								break;																							
-							default:
-								$tabs_navigation .= 'Unknown';
-								break;
-						}
-						$tabs_navigation .= '</li>';					
-					}
-				} // END - foreach( $tabs_content as $key => $value )
-			}
-			
-			// Only print the tabs functionality if the buttons are set
-			if ( $tabs_navigation ) {
-				echo '<ul class="tabber-navigation">' . $tabs_navigation . '</ul>';
-				echo $tabs_content;
-			}
-		
-		?>
-		
-		
-	</div>
-	
-	<div style="clear:both;"></div>
 
 	<div id="research-center-goodreads" class="research-center-info-zone">
 
@@ -118,6 +121,8 @@ Template Name: Page - Research Center
 	<div id="gr_grid_widget_1236291550">
 		
 	</div>
+	
+	<script src="http://www.goodreads.com/review/grid_widget/2095476.Featured%20Books?num_books=22&amp;order=d&amp;shelf=read&amp;sort=date_added&amp;widget_id=1236291550" type="text/javascript" charset="utf-8"></script>
 		
 	<?php endif; ?>
 
@@ -147,6 +152,7 @@ Template Name: Page - Research Center
 		
 		cunyj_load_blog_posts( 'http://researchcenter.journalism.cuny.edu/', 6, 'research-center-blog' );
 		
+		jQuery('ul.tabber-navigation').show();
 		jQuery('ul.tabber-navigation li:eq(0)').addClass('active');
 		jQuery('div.tabber-item:eq(0)').addClass('active');
 		
@@ -163,7 +169,6 @@ Template Name: Page - Research Center
 	});
 	
 </script>
-<script src="http://www.goodreads.com/review/grid_widget/2095476.Featured%20Books?num_books=22&amp;order=d&amp;shelf=read&amp;sort=date_added&amp;widget_id=1236291550" type="text/javascript" charset="utf-8"></script>
 
 <?php get_footer(); ?>
 
