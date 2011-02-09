48ff4e916511f5e13d4d7d2c03307fe6f68b7449
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-02-09 22:17:13 +0000

    Page anchors on the database search page

--- type-cunyj_database.php
+++ type-cunyj_database.php
@@ -1,5 +1,32 @@
 <?php get_header(); ?>
 
+	<?php
+		$search_query = ( $_GET['s'] ) ? $_GET['s'] : false;
+		$args = array(	'order' => 'ASC',
+						'nopaging' => true,
+						'posts_per_page' => '-1',
+						's' => $search_query,
+						'post_type' => 'cunyj_database',
+						);
+		$databases = new WP_Query( $args );
+		
+		$page_anchors = array();
+		if ( $databases->have_posts() ) {
+			while ( $databases->have_posts() ) {
+				$databases->the_post();
+				$first_letter = substr( strtolower( get_the_title() ), 0, 1 );
+				$page_anchors[$first_letter] = true;
+			}
+		} // END if ( $databases->have_posts() )
+		
+		$page_anchors_html = '';
+		foreach ( $page_anchors as $letter => $value ) {
+			$page_anchors_html .= '<a href="#' . $letter . '">' . strtoupper( $letter ) . '</a> | ';
+		}
+		$page_anchors_html = rtrim( $page_anchors_html, '| ' );
+		
+	?>
+
 <div class="wrap">
 
 <div class="main" id="databases">
@@ -11,26 +38,25 @@
 		        <button class="search-button" type="submit">Search</button>
 		       </div>
 		</form>
+		<?php if ( count( $page_anchors ) > 1 ): ?>
+		<p class="jump">Or jump to <?php echo $page_anchors_html; ?></p>
+		<?php endif; ?>
 	</div>
 
 <div class="content" id="results">
-	
-	<?php
-		$search_query = ( $_GET['s'] ) ? $_GET['s'] : false;
-		$args = array(	'order' => 'ASC',
-						'nopaging' => true,
-						'posts_per_page' => '-1',
-						's' => $search_query,
-						'post_type' => 'cunyj_database',
-						);
-		$databases = new WP_Query( $args );
-	?>
   
-	<?php if ( $databases->have_posts()) : while ( $databases->have_posts() ) : $databases->the_post();
+	<?php if ( $databases->have_posts() ) : while ( $databases->have_posts() ) : $databases->the_post();
 		$post_id = get_the_id();
 		$database_link = get_post_meta( $post_id, '_cunyj_databases_database_link', true );
 		$tutorial_link = get_post_meta( $post_id, '_cunyj_databases_tutorial_link', true );
 		$topics = wp_get_post_terms( $post_id, 'cunyj_database_topics' );
+		
+		// Add a page anchor for the first in the series
+		$first_letter = substr( strtolower( get_the_title() ), 0, 1 );
+		if ( $page_anchors[$first_letter] ) {
+			echo '<a name="' . $first_letter . '"></a>';
+			$page_anchors[$first_letter] = false;
+		}
 	?>
     <div class="database" id="database-<?php the_ID(); ?>">
 	
