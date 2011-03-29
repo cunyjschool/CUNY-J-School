79e91a431ae308962339009236f2fa8cbcafc94a
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-03-29 17:57:03 +0000

    Remove link from event title

--- single-cunyj_event.php
+++ single-cunyj_event.php
@@ -16,7 +16,7 @@
     
 	<div class="cunyj_event" id="cunyj_event-<?php the_ID(); ?>">
 
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<h2><?php the_title(); ?></h2>
 		
 			<?php
 			
