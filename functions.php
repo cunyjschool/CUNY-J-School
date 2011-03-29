2fd162097bb24da1d473f2cdaf0fab79f2319362
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-03-29 18:10:06 +0000

    Fix styling on creating events standardize queueing resource files

--- functions.php
+++ functions.php
@@ -40,26 +40,25 @@ class cunyj
 
 		if ( is_admin() ) {
 			add_action( 'admin_menu', array(&$this, 'add_admin_menu_items') );
-			wp_enqueue_style( 'cunyj_events_admin', get_bloginfo('template_directory') . '/css/events_admin.css', array( 'cunyj_primary' ), CUNYJ_VERSION );
 		} else {
 			// Enqueue our stylesheets
-			wp_enqueue_style( 'cunyj_primary', get_bloginfo('template_directory') . '/style.css', false, CUNYJ_VERSION );
-			wp_enqueue_style( 'cunyj_sidebar', get_bloginfo('template_directory') . '/css/sidebar.css', array('cunyj_primary'), CUNYJ_VERSION );
-			wp_enqueue_style( 'cunyj_media', get_bloginfo('template_directory') . '/css/media.css', array('cunyj_primary'), CUNYJ_VERSION );
-			wp_enqueue_style( 'cunyj_buddypress', get_bloginfo('template_directory') . '/css/buddypress.css', array('cunyj_primary'), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_primary_css', get_bloginfo('template_directory') . '/style.css', false, CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_sidebar_css', get_bloginfo('template_directory') . '/css/sidebar.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_media_css', get_bloginfo('template_directory') . '/css/media.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_buddypress_css', get_bloginfo('template_directory') . '/css/buddypress.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
 			
-			wp_enqueue_style( 'cunyj_home', get_bloginfo('template_directory') . '/css/home.css', array('cunyj_primary'), CUNYJ_VERSION );
-			wp_enqueue_style( 'cunyj_nextgen_gallery', get_bloginfo('template_directory') . '/css/nextgen_gallery.css', array('cunyj_primary'), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_home_css', get_bloginfo('template_directory') . '/css/home.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_nextgen_gallery_css', get_bloginfo('template_directory') . '/css/nextgen_gallery.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
 			
-			wp_enqueue_style( 'cunyj_databases', get_bloginfo('template_directory') . '/css/databases.css', array( 'cunyj_primary' ), CUNYJ_VERSION );
-			wp_enqueue_style( 'events', get_bloginfo('template_directory') . '/css/events.css', array( 'cunyj_primary' ), CUNYJ_VERSION );
-			wp_enqueue_style( 'cunyj_capstones', get_bloginfo('template_directory') . '/css/capstones.css', array( 'cunyj_primary' ), CUNYJ_VERSION );			
+			wp_enqueue_style( 'cunyj_databases_css', get_bloginfo('template_directory') . '/css/databases.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_events_css', get_bloginfo('template_directory') . '/css/events.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );
+			wp_enqueue_style( 'cunyj_capstones_css', get_bloginfo('template_directory') . '/css/capstones.css', array( 'cunyj_primary_css' ), CUNYJ_VERSION );			
 			
 			// Load in header
-			wp_enqueue_script( 'cunyj_main', get_bloginfo('template_directory') . '/js/main.js', array('jquery'), CUNYJ_VERSION );
+			wp_enqueue_script( 'cunyj_main_js', get_bloginfo('template_directory') . '/js/main.js', array( 'jquery' ), CUNYJ_VERSION );
 
 			// Load in footer
-			wp_enqueue_script( 'cunyj_live', get_bloginfo('template_directory') . '/js/live.js', array('jquery'), CUNYJ_VERSION, true );
+			wp_enqueue_script( 'cunyj_live_js', get_bloginfo('template_directory') . '/js/live.js', array( 'jquery' ), CUNYJ_VERSION, true );
 		}
 		
 		// Register all of our navigation menus
