7f6ad0183197b7d5eb3214bcd33ffb801261c368
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-05 13:57:04 +0000

    Create a new page for the CUNY Campus Wire

---
+++ page-cunycampuswire.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Page - CUNY Campus Wire
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="wrap">
+
+<div class="main">
+	
+	<h2>CUNY Campus Wire</h2>
+
+<div class="sidebar left">
+
+	<ul>
+		<li>
+			<h4><a href="http://www.thebridgenews.com/">The Bridge</a></h4>
+			<p>LaGuardia Community College</p>
+		</li>
+		<li>
+			<h4><a href="http://www.bronxnet.org/">Bronx Net</a></h4>
+			<p>Lehman College</p>
+		</li>
+	</ul>
+	
+</div><!-- END .sidebar -->
+          
+
+<div class="clear-both"></div>
+
+</div><!-- END .main -->
+
+</div><!-- END .wrap -->
+
+<?php get_footer(); ?>
+
