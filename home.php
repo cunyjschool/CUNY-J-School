fb64685f9dac2d633b4cb56cb6d5c7124d182084
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-21 22:32:23 +0000

    Work on the homepage

--- home.php
+++ home.php
@@ -4,6 +4,42 @@
 	
 	<div class="wrap">
 		
+		<div class="content">
+			
+			<div id="homepage-headlines-section" class="section">
+				
+			<?php
+			$args = array(
+				'category_name' => 'featured-news',
+				'showposts' => 5,
+			);
+			$homepage_headlines = new WP_Query( $args );
+			?>
+		
+			<div class="float-right homepage-headlines">
+				<h3><a href="#">Headlines</a></h3>
+				<ul>
+				<?php if ( $homepage_headlines->have_posts() ) : ?>
+				<?php while ( $homepage_headlines->have_posts() ) : $homepage_headlines->the_post(); ?>
+			  		<li><h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4></li>
+				<?php endwhile; else: ?>
+				<li>There are currently no stories.</li>
+				<?php endif; ?>
+				</ul>
+			</div>
+			
+			<div class="homepage-headlines-gallery">
+				
+				
+			</div>
+			
+			</div><!-- .section#homepage-headlines-section -->
+			
+			<div class="section">
+				
+			</div>
+		
+		</div><!-- END .content -->
 		
 	</div><!-- END .wrap -->
 	
---
+++ sidebar-home.php
@@ -0,0 +1,8 @@
+<div id="sidebar-home" class="sidebar float-right primary-sidebar">
+	
+	<div class="sidebar-item">
+		<h4><a href="#">Headlines</a></h4>
+		
+	</div>
+	
+</div><!-- END .sidebar.float-right.primary-sidebar#sidebar-home -->
