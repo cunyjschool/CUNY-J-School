7d22da52cd77b1985113c708c6c94b2adf2ce54d
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-21 21:27:36 +0000

    Add a Google Map image and link to the footer

--- footer.php
+++ footer.php
@@ -2,7 +2,9 @@
 	
 	<div class="wrap primary-footer">
 		
-		<a href="http://www.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/images/logos/cuny-cube_s30.jpg" height="30px" width="30px" class="float-right" id="cuny-footer-logo" alt="CUNY" />
+		<div class="google-map float-right">
+			<a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=219+W+40th+St,+New+York,+NY+10018&aq=0&sll=40.753564,-73.98644&sspn=0.01024,0.022638&ie=UTF8&hq=&hnear=219+W+40th+St,+New+York,+10018&ll=40.755116,-73.986343&spn=0.005323,0.011319&z=17" title="Get directions to the CUNY Graduate School of Journalism on Google Maps"><img src="<?php bloginfo('template_directory'); ?>/images/logos/cuny-cube_s30.jpg" height="30px" width="30px" class="float-right" id="cuny-footer-logo" alt="CUNY" /></a>
+		</div>
 			
 		<h4><a href="<?php bloginfo( 'url' ); ?>">CUNY Graduate School of Journalism</a></h4>
 		
@@ -16,10 +18,14 @@
 			wp_nav_menu( $args );
 		?>
 		
+		<p class="contact">219 W. 40th Street, New York, NY 10018 | (646) 758-7800</p>
+		
+		<div class="clear-both"></div>
+		
 	</div><!-- END .wrap -->
 	
 	<div class="wrap secondary-footer">
-		<p>&copy;<?php echo date( 'Y' ); ?> CUNY Graduate School of Journalism</p>
+		<p>&copy; <?php echo date( 'Y' ); ?> CUNY Graduate School of Journalism</p>
 	</div>
 	
 </div><!-- END .footer -->
