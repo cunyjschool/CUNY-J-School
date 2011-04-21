8ad8afa48516e2ec2553c955f0584e75af8da2a1
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-21 19:15:40 +0000

    Reset the footer

--- footer.php
+++ footer.php
@@ -1,25 +1,10 @@
-<div id="footer">
-  <div class="wrap">
+<div class="footer">
 	
-	<div id="footerwrap">
-  
-  <?php if(is_page(1146) || $post->post_parent=="1146" ) { ?>  	
-<a href="http://www1.cuny.edu/news/alert.html"><img src="<?php bloginfo('template_directory'); ?>/img/cuny-alert.jpg" style="float: right;" alt="CUNY A!ert" /></a>
-  <?php } ?>
-  
-<a href="http://www.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/images/logos/cuny-cube_s30.jpg" height="30px" width="30px" style="float: right;" alt="CUNY" /></a>
-  
-
-&copy; <?php echo date('Y'); ?> <a href="http://www.journalism.cuny.edu/">Graduate School of Journalism</a> | <a href="http://cuny.edu/">City University of New York</a><br />
-219 W. 40th Street | New York, NY 10018 | <a href="<?php bloginfo('url'); ?>/about/map-and-directions/">Map and Directions</a> | <a href="<?php bloginfo('url'); ?>/about/campus-facilities/hours-of-operation/">Hours of Operation</a><br />
-<a href="http://www.cuny.edu/about/administration/offices/ehsrm/flu.html">H1N1 Info</a> | <a href="<?php bloginfo('url'); ?>/contact-us/">Contact Us</a> | <a href="http://www.cuny.edu/employment.html">Employment</a> | <a href="https://cunyjschool.wufoo.com/forms/web-feedback-form/">Web Feedback</a> | <a href="http://www.browsealoud.com/page.asp?pg_id=80096" rel="nofollow">Listen To This Website</a> | <a href=" 
-http://assistive.usablenet.com/tt/<?php bloginfo('url'); ?>">Text-Only</a>
-
-  </div>
-
-</div>
-
-</div>
+	<div class="wrap">
+		
+	</div><!-- END .wrap -->
+	
+</div><!-- END .footer -->
 
 <!-- Clicky analytics tag -->
 <?php if ( get_bloginfo('url') == 'http://www.journalism.cuny.edu' ) : ?>
@@ -31,11 +16,9 @@ http://assistive.usablenet.com/tt/<?php bloginfo('url'); ?>">Text-Only</a>
 	<script type="text/javascript">clicky.init(263275);</script>
 	<noscript><p><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/263275ns.gif" /></p></noscript>
 <?php endif; ?>
-<!-- End Clicky analytics -->
+<!-- END Clicky analytics -->
 
-  <?php wp_footer(); ?>
-
-	<!-- New site ^DB 7/27 -->
+	<?php wp_footer(); ?>
         
 </body>
 </html>
