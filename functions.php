87969d47c06553dfca51b1a6f98ec74d1d228f91
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-25 19:17:05 +0000

    Tagging v1.3.5

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 //define( 'CUNYJ_THEME_URL' , themes_url(themes_basename(dirname(__FILE__)).'/') );
 define( 'CUNYJ_PREFIX' , 'cunyj_' );
-define( 'CUNYJ_VERSION', '1.3.4' );
+define( 'CUNYJ_VERSION', '1.3.5' );
 
 include_once('php/cunyj_events.php');
 include_once('php/cunyj_databases.php');
