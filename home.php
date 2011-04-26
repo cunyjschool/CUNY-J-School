906c6278517f182d8c648484f77fad8df9ec3cfc
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-26 20:04:57 +0000

    More work on the top of the homepage

--- home.php
+++ home.php
@@ -6,36 +6,50 @@
 		
 		<div class="content">
 			
-			<div id="homepage-headlines-section" class="section">
+			<div id="home-page-item-headlines" class="home-page-item row">
 				
-			<?php
-			$args = array(
-				'category_name' => 'featured-news',
-				'showposts' => 5,
-			);
-			$homepage_headlines = new WP_Query( $args );
-			?>
+				<?php
+				$args = array(
+					'category_name' => 'featured-news',
+					'showposts' => 4,
+				);
+				$homepage_headlines = new WP_Query( $args );
+				?>
 		
-			<div class="float-right homepage-headlines">
-				<h3><a href="#">Headlines</a></h3>
-				<ul>
-				<?php if ( $homepage_headlines->have_posts() ) : ?>
-				<?php while ( $homepage_headlines->have_posts() ) : $homepage_headlines->the_post(); ?>
-			  		<li><h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4></li>
-				<?php endwhile; else: ?>
-				<li>There are currently no stories.</li>
-				<?php endif; ?>
-				</ul>
-			</div>
-			
-			<div class="homepage-headlines-gallery">
+				<div class="homepage-headlines">
+					<h3 class="latest-headlines-title"><a href="#">Latest Headlines</a></h3>
+					<ul class="latest-headlines">
+					<?php if ( $homepage_headlines->have_posts() ) : ?>
+					<?php while ( $homepage_headlines->have_posts() ) : $homepage_headlines->the_post(); ?>
+				  		<li><h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4></li>
+					<?php endwhile; else: ?>
+						<li>There aren't any stories currently.</li>
+					<?php endif; ?>
+					</ul>
+				
+					<h3><a href="#">How to Apply</a></h3>
 				
 				
-			</div>
+				</div>
 			
-			</div><!-- .section#homepage-headlines-section -->
+				<div class="homepage-headlines-gallery">
+				
+					<div class="homepage-headlines-excerpt">
+						This is sample text that will appear as an overlay on the image. We can include a sentence or two, and then link to the story.
+					</div>
+				
+					<img src="http://www.danielbachhuber.com/screenshots/20110424-newsgaminghack_db-1241_-_Version_2-20110426-144649.jpg" height="400px" width="600px" />
+				
+				</div>
 			
-			<div class="section">
+			</div><!-- .home-page-item#homepage-headlines-section -->
+			
+			<div class="row">
+				
+				<div id="home-page-item-events" class="home-page-item">
+					
+				
+				</div>
 				
 			</div>
 		
