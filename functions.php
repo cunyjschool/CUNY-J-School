5c36a1cb05fdb0191ed9b9ba527ab537b2822d97
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-31 18:43:18 +0000

    Bump to v1.3.8b

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'CUNYJ_PREFIX' , 'cunyj_' );
-define( 'CUNYJ_VERSION', '1.3.8a' );
+define( 'CUNYJ_VERSION', '1.3.8b' );
 
 include_once('php/cunyj_events.php');
 include_once('php/cunyj_databases.php');
