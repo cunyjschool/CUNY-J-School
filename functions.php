a87f37022242ad0cec54aee3b5c2d8d00cab431b
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-07-13 17:06:30 +0000

    Bump to v1.4a for deploy to production

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'CUNYJ_PREFIX' , 'cunyj_' );
-define( 'CUNYJ_VERSION', '1.3.8b' );
+define( 'CUNYJ_VERSION', '1.4a' );
 
 include_once('php/cunyj_events.php');
 include_once('php/cunyj_databases.php');
